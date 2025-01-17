0675540d86bd6ab4660e0fe372d29b863b3d3ac5
Julia Anisio <julia.an.nogueira@gmail.com>
2025-01-17 11:42:01 -0300

    service layer, component registration

---
+++ src/main/java/com/estudospring/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.estudospring.course.services;
+
+import com.estudospring.course.entities.User;
+import com.estudospring.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
